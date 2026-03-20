a69e9693809a1a04edfdce87e06ff87448762b8d
Jhojan Camilo Rodriguez Castañeda <jhojancamilorodriguez2017@gmail.com>
2026-03-19 22:08:25 -0500

    Añadi explicacion listas

---
+++ 32_Listas.py
@@ -0,0 +1,27 @@
+#Listas
+my_list = list()
+my_other_list = []
+
+my_list = [35, 32, 20, 11, 15]
+
+print(my_list)
+print(len(my_list))
+
+my_other_list = [35, 1.77, "Camilo"]
+
+print(type(my_other_list))
+
+print(my_other_list[1])
+
+a, b, c, = my_other_list
+print(c)
+
+print(my_other_list, my_list)
+
+my_other_list.append("Colombia")
+
+my_other_list.insert(1, "Azul")
+
+my_other_list.remove("Azul")
+
+print(my_other_list)
