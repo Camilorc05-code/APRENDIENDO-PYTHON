bef79a235f1fef0da3758a79b8d0fcf3eec5c3ff
Jhojan Camilo Rodriguez Castañeda <jhojancamilorodriguez2017@gmail.com>
2026-03-23 21:40:41 -0500

    AGREGUE CLASE CONDICIONALES

---
+++ 36_CONDICIONALES.py
@@ -0,0 +1,22 @@
+## CONDICIONALES ##
+
+my_condition = False
+
+if my_condition:
+    print("Se ejecuta la condición del if")
+
+my_condition = 1
+
+if my_condition == 10:
+    print("Se ejecuta la condición del if")
+
+if my_condition >= 10 and my_condition <= 20:
+    print("es mayor que 10 y menor que 20")
+elif my_condition == 1:
+    print("Es igual a 1")
+else :
+    print("Es menor o igual que 10 o mayor o igual de 20")
+
+
+print("La ejecución continua")
+
