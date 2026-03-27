f3ac0fc52f486388623133823edcda6b9bc5e3b3
Jhojan Camilo Rodriguez Castañeda <jhojancamilorodriguez2017@gmail.com>
2026-03-27 09:32:39 -0500

    AGREGUE CLASES

--- 39_CLASES.py
+++ 39_CLASES.py
@@ -8,7 +8,11 @@ print(MyPerson())
 
 class Person:
     def __init__(self, name, surname, alias = "Sin alias"):
-        self.full_name = f"{name} {surname} ({alias})"
+        self.full_name = f"{name} {surname} ({alias})" #Propiedad publica
+        self.__name = name #Propiedad privada
+
+    def get_name (self):
+        return self.__name
 
     def walk (self):
         print(f"{self.full_name} esta caminando")
@@ -16,6 +20,7 @@ class Person:
 
 my_person = Person("Camilo", "Rodriguez")
 print(my_person.full_name)
+print(my_person.get_name())
 my_person.walk()
 
 my_other_person = Person("Camilo", "Rodriguez", "RC_05")
