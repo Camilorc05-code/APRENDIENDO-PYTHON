49085aee7e7112fefb15a2f0cc570aefe6216f2c
Jhojan Camilo Rodriguez Castañeda <jhojancamilorodriguez2017@gmail.com>
2026-03-27 09:24:17 -0500

    AGREGUE CLASES

---
+++ 39_CLASES.py
@@ -0,0 +1,31 @@
+### CLASES ###
+
+class MyPerson: 
+    pass
+
+print(MyPerson)
+print(MyPerson())
+
+class Person:
+    def __init__(self, name, surname, alias = "Sin alias"):
+        self.full_name = f"{name} {surname} ({alias})"
+
+    def walk (self):
+        print(f"{self.full_name} esta caminando")
+       
+
+my_person = Person("Camilo", "Rodriguez")
+print(my_person.full_name)
+my_person.walk()
+
+my_other_person = Person("Camilo", "Rodriguez", "RC_05")
+print(my_other_person.full_name)
+my_other_person.walk()
+my_other_person.full_name = "Felipe es inteligente"
+print(my_other_person.full_name)
+
+my_other_person.full_name = 2342
+print(my_other_person.full_name)
+
+
+
