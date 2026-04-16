7488ff514aa06bb8d801d6365a0401df131d074d
Jhojan Camilo Rodriguez Castañeda <jhojancamilorodriguez2017@gmail.com>
2026-04-15 21:12:23 -0500

    AGREGUE CHALLENGES

--- Intermediate/02_Challenges.py
+++ Intermediate/02_Challenges.py
@@ -43,12 +43,13 @@ def is_anagrama(word_one, word_two):
 print(is_anagrama("Amor", "Roma"))
 
 """
+"EJERCICIO FINOBACCI"
 Escribe un programa que imprima los 50 primeros números de la suseción 
 de Fibonacci empezando en 0.
 - La serie Fibonacci se compone por una sucesión de número en
 la que el siguiente siempre es la suma de los dos anteriores.
 """
-
+print("EJERCICIO FINOBACCI")
 def fibonacci():
 
   prev = 0
@@ -61,6 +62,50 @@ def fibonacci():
     next = fib
 
 fibonacci()
-  
+
+"""
+¿ES UN NÚMERO PRIMO?
+Escribe un programa que se encargue de comprobar si un número es o no primo.
+Hecho esto, imprime los números primos entre 1 y 100.
+"""
+print("¿ES UN NÚMERO PRIMO?")
+
+def es_primo():
+    
+    for number in range(1, 101):
+
+      if number >= 2:
+
+        es_divisible = False
+      
+        for index in range(2,number):
+            if number % index == 0:
+              es_divisible = True
+              break
+
+        if not es_divisible:      
+           print(number)
+    
+es_primo()
+
+"""
+INVIRTIENDO CADENAS
+Crea un programa que invierta el orden de una cadena de texto
+sin usar funciones propias del lenguaje  que lo hagan de forma automatica.
+- Si le pasamos "Hola mundo" nos retornaria "odnum aloH"
+"""
+
+def reverse(text):
+  text_len = len(text) 
+  reverse_text = ""
+  for index in range(0, text_len):
+    reverse_text += text[text_len - index -1]
+
+  return reverse_text
+
+print(reverse("Hola mundo"))
+
+
+
 
 
