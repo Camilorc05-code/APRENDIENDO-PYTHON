e59676acee5e2fc4c1e0de8e72b57aff16c97fd7
Jhojan Camilo Rodriguez Castañeda <jhojancamilorodriguez2017@gmail.com>
2026-05-05 21:12:04 -0500

    AGREGUE CLASE API

--- Backend/FastAPI/main.py
+++ Backend/FastAPI/main.py
@@ -6,6 +6,16 @@ app = FastAPI()
 async def root():
     return "¡Hola FastAPI.!"
 
+# Url local: http://127.0.0.1:8000
+
 @app.get("/url")
 async def root():
     return {"url_curso":"https://mouredev.com/python"}
+
+# Url local (/url): http://127.0.0.1:8000/url
+
+# Inicia el server: uvicorn Backend.FastAPI.main:app --reload
+# Detener el server: CTRL+C
+
+#Documentación con Swagger: http://127.0.0.1:8000/docs
+# Documentación con Redocly: http://127.0.0.1:8000/redoc
