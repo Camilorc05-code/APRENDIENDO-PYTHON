42198f152226fb9af72b5766ce3fa921c5b33e9c
Jhojan Camilo Rodriguez Castañeda <jhojancamilorodriguez2017@gmail.com>
2026-05-14 21:22:20 -0500

    CLASE JWT

--- Backend/FastAPI/routers/jwt_auth_users.py
+++ Backend/FastAPI/routers/jwt_auth_users.py
@@ -1,14 +1,15 @@
-from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status
 from pydantic import BaseModel
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from jose import jwt 
+from jose import jwt, JWTError
 from passlib.context import CryptContext
 from datetime import datetime, timedelta, timezone
 
 ALGORITHM = "HS256"
 ACCESS_TOKEN_DURATION = 1
+SECRET = "80b3a09b367733cd69fa825f53df84ce1c7d9359a76c025e6bb0ba9c1a2f70d8"
 
-app = FastAPI()
+router = APIRouter()
 
 oauth2 = OAuth2PasswordBearer(tokenUrl="login")
 
@@ -43,8 +44,37 @@ users_db = {
 def search_user_db(username: str):
     if username in users_db:
         return UserDB(**users_db[username])
+    
+def search_user(username: str):
+    if username in users_db:
+        return User(**users_db[username])
 
-@app.post("/login")
+async def auth_user(token: str = Depends(oauth2)):
+  
+  exception = HTTPException(
+            status.HTTP_401_UNAUTHORIZED, 
+            detail="Credenciales de autentificación invalidas", 
+            headers={"WWW-Authenticate": "Bearer"})
+
+  try:
+    username = jwt.decode(token, SECRET, algorithms=[ALGORITHM]).get("sub")
+    if username is None:
+       raise exception
+
+  except JWTError:
+    raise exception
+  
+  return search_user(username)
+    
+async def current_user(user: User = Depends(auth_user)): 
+    if user.disable:
+        raise HTTPException(
+            status.HTTP_400_BAD_REQUEST, 
+            detail="Usuario inactivo")
+
+    return user
+
+@router.post("/login")
 async def login(form: OAuth2PasswordRequestForm = Depends()):
    user_db = users_db.get(form.username)
    if not user_db:
@@ -60,6 +90,10 @@ async def login(form: OAuth2PasswordRequestForm = Depends()):
    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_DURATION)
    
    access_token = {"sub": user.username, 
-                   "exp": expire.isoformat()}
+                   "exp": int(expire.timestamp())}
    
-   return {"access_token": access_token, "token_type": "bearer"}
+   return {"access_token": jwt.encode(access_token, SECRET, algorithm=ALGORITHM), "token_type": "bearer"}
+
+@router.get("/users/me")
+async def me(user: User = Depends(current_user)):
+    return user
